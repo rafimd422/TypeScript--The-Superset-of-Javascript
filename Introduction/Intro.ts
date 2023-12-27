2776cd8da2312248f5192704568fe11ea99d9a2a
rafimd422 <rafimd2222@gmail.com>
2023-12-28 01:47:29 +0600

    added introduction of typescript

--- Introduction/Intro.ts
+++ Introduction/Intro.ts
@@ -1,2 +1,36 @@
-// What is TypeScript? 
-// - TypeScript is a Superset of Javascript which is Created by Microsoft
+// Installation of TypeScript
+ // Step 1: Make sure Node.js is available in your mechine.
+ // Step 2: Install TypeScript compiler using: npm i -g typescript
+
+
+// What is TypeScript?
+
+// - TypeScript is a Superset of Javascript which is Created by Microsoft.
+// - It Allows statuc strict typing.
+// - Extra features: Interfaces, enums, tuples, generucs.
+// - Support modern Features(Arrow functions, let, const)
+// Typescript is based on the .NET harmony specification.
+
+
+
+
+
+// What was wrong with javascript?
+
+// - Javascript is Not suitable for large applcations.
+// - Javascript has no strong typing.
+// - Weired inheritance, unfamiliar syntax.
+// - Only errors During runtime where typescript will show the error on the compilation time.
+// - Suffers Type coercion.
+
+
+
+
+
+
+// Benefits of TypeScript
+
+// - TypeScript = Javascript
+// - Compiles to Javascript - runs everyWhere.
+// - Familiar with Object Oriented Programming(OOP).
+// - New Features of javascript work everywhere in typeScript.
