c83e08cec473dac2f6c877e7336de13b99b08ecd
rafimd422 <rafimd2222@gmail.com>
2024-01-04 23:21:02 +0600

    showcased a normal error to understand the time it strictly maintain it's type

---
+++ TypeBasic/TypeBasic.ts
@@ -0,0 +1,7 @@
+let playerName = "Mashrafi"
+
+console.log(playerName) // Output: Mashrafi
+
+playerName = 34; // it is showing error because it already assume it as String. so, it will follow this type strictly.
+
+console.log(playerName)
