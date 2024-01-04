9b063d8ede82ee2941aa5b61196af71a410a2fce
rafimd422 <rafimd2222@gmail.com>
2024-01-04 23:23:51 +0600

    added the time when it will happen only

--- TypeBasic/TypeBasic.ts
+++ TypeBasic/TypeBasic.ts
@@ -3,5 +3,8 @@ let playerName = "Mashrafi"
 console.log(playerName) // Output: Mashrafi
 
 playerName = 34; // it is showing error because it already assume it as String. so, it will follow this type strictly.
-
 console.log(playerName)
+
+// It will only work when you declare and define a value at a time.
+
+
