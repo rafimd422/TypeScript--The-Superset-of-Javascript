0b7566834d5dc7e64f3cdcafc2ecce9b95596bd3
rafimd422 <rafimd2222@gmail.com>
2024-01-05 17:13:01 +0600

    added the type insertion exapmle

--- TypeBasic/TypeBasic.ts
+++ TypeBasic/TypeBasic.ts
@@ -1,3 +1,5 @@
+// one
+
 let playerName = "Mashrafi"
 
 console.log(playerName) // Output: Mashrafi
@@ -7,4 +9,24 @@ console.log(playerName)
 
 // It will only work when you declare and define a value at a time.
 
+
+
+
+// two
+let NameOfPlayer;
+
+NameOfPlayer = 'Mashrafi'
+NameOfPlayer = 34;
+
+console.log(NameOfPlayer) // Output: 34
+// Output is 34 Because it didn't declare and define the value of variable at a time.
+
+
+
+
+// three
+function multiply(a:number ,b:number){
+    return a * b;
+}
+         // we can only set a number on the parameter. because we already set the type. and it only possible in typescript
 
