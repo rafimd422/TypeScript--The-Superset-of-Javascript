d250008c351547007302ed3aa08625b85cf5f2c6
rafimd422 <rafimd2222@gmail.com>
2024-01-06 23:23:24 +0600

    added the array ecplicit and union type

--- Explicit & Union Types/ExplicitAndUnion.ts
+++ Explicit & Union Types/ExplicitAndUnion.ts
@@ -1,3 +1,16 @@
 // To explicitly make a variable only for string 
-let a:string;
-         // Now a can only assign value which is string. It will never accept a value which is a number.
+let a: string;
+         // Now a can only assign value which is string.
+         // It will never accept a value which is a number.
+
+
+// in Array
+let b:string[] = []
+// here we can only push/add string on our array.
+// the purpose of using [] as the value is because we can't push anything without assigning this value.
+
+
+// if we want to add a string and number both in our array, we can use it such as, 
+let c:(string | number)[] = []
+
+c.push('sumit', 45)
