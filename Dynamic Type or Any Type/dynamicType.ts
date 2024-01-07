9eb421cac7dd255a60f161754fde78e4956a250a
rafimd422 <rafimd2222@gmail.com>
2024-01-07 23:22:13 +0600

    added the example with dynamic type or any type with short description

---
+++ Dynamic Type or Any Type/dynamicType.ts
@@ -0,0 +1,14 @@
+let e: any;
+// when we need multiple type of we can access any types on the variable. then we can write like this. which means it can be any types.
+e = 5;
+e= 'Rafi' 
+
+// this type will use in particular time. only when we need to add any types of value.
+// But it use rarely
+
+
+// In Object
+let f:{
+name: any,
+age:any
+}
