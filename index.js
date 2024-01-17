cb703744e35c626a5763227829055bba259d2642
rafimd422 <rafimd2222@gmail.com>
2024-01-18 03:14:02 +0600

    just some practice from anisul islam sir playlisy

---
+++ index.js
@@ -0,0 +1,9 @@
+// ts from sumit saha playlist
+var Name;
+Name = "Rafi";
+console.log(Name);
+var sum = function (num1, num2) {
+    var totalSum = num1 + num2;
+    return totalSum;
+};
+console.log(sum(8, 2));
---
+++ index.ts
@@ -0,0 +1,13 @@
+// ts from sumit saha playlist
+
+let Name:string; 
+Name = "Rafi"
+console.log(Name)
+ 
+
+const sum = (num1:number, num2:number) => {
+const totalSum = num1 + num2;
+return totalSum;
+}
+console.log(sum(8,2))
+ 
