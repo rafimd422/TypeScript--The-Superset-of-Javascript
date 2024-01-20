b430ee5ba9715b46330a5f9ba578f98bdc9905f8
rafimd422 <rafimd2222@gmail.com>
2024-01-21 02:43:05 +0600

    added new instruction

--- Type Aliases/typeAlias.ts
+++ Type Aliases/typeAlias.ts
@@ -1,5 +1,5 @@
 // the type keyword use for store our variable/params types.
-
+// It is use to make custom types and use more then one variable/params
 
 type stringOrNum = string | number;
 type userType = {name:string, age:number};
