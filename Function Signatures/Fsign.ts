4daea5dd1e3929c4318b5289203f9b253f3fb2f8
rafimd422 <rafimd2222@gmail.com>
2024-01-22 14:18:05 +0600

    learned how to add default return type for function from sumit saha playlist

---
+++ Function Signatures/Fsign.ts
@@ -0,0 +1,6 @@
+let myFunc: (x:string , y:string) => void; //here, void is defalut type of return value of the function.
+                            // we can write number or string or boolean also instead of void. 
+
+myFunc = (a:string,b:string) =>{
+    return a + b
+}
