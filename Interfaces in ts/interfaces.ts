1ee948c8097830e84e43d257430fd134857c5114
rafimd422 <rafimd2222@gmail.com>
2024-01-23 13:18:29 +0600

    learned how to implement interface on function

--- Interfaces in ts/interfaces.ts
+++ Interfaces in ts/interfaces.ts
@@ -1,1 +1,17 @@
 // Interfaces are a feature of TypeScropt what allows us to define structure or shap of an object and specify the properties and methods that an object has or should have 
+
+
+// implement interface on function
+interface RectanglOptions {
+    width:number;
+    length:number;
+}
+
+function drowRectangle(options:RectanglOptions){
+    let width = options.width;
+    let length = options.length;
+    return width + '' + length
+}
+
+drowRectangle({width:54, length:75})
+
