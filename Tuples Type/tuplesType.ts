4099f703b4a50741d9858edc7c14e697a807c5db
rafimd422 <rafimd2222@gmail.com>
2024-01-24 00:29:39 +0600

    added tuples type example using let

--- Tuples Type/tuplesType.ts
+++ Tuples Type/tuplesType.ts
@@ -1,3 +1,9 @@
 // A tuple is a typed array with a pre-defined length and types for each index.
 
+// with let 
+let c:[number, string, object]
+c = [4, 'It is string', {a:'142', b:420}]
+
+// with const 
 const b:[number, string, object] = [4, 'It is string', {a:'142', b:420}]
+
