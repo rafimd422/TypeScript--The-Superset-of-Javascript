3754d80085099b3b5e26fc2c654ec6f3de9b8942
rafimd422 <rafimd2222@gmail.com>
2024-01-23 21:04:08 +0600

    Added a example of generics using interface of typescript

--- Generics in Ts/generics.ts
+++ Generics in Ts/generics.ts
@@ -1,1 +1,16 @@
 // Generics is a reuseable block of code which we can use for types.
+
+// Generic with interface:
+interface APIResponse<T> {
+    status: number;
+    type: string;
+    data: T;
+}
+
+const response:APIResponse<object> = {
+status:200,
+type:'Good',
+data:{
+    something:'something'
+}
+} // here we just pass a generic sign(<T>) with interface and defined the type on the response variable
