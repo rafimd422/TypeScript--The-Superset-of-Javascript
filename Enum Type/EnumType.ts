4d796c7b99a0b3c4643f5c77799eace0fdd54af6
rafimd422 <rafimd2222@gmail.com>
2024-01-24 00:09:37 +0600

    added a example of enum types

--- Enum Type/EnumType.ts
+++ Enum Type/EnumType.ts
@@ -1,2 +1,17 @@
 // The enum type help us to store constant's. It does not allow duplicate value.
 // Enum types can be numeric, string , hetergenous
+
+enum RType {SUCCESS, FAILURE, UNAUTHENTICATED, FORBIDDEN}
+
+interface APIResponsee<T> {
+    status:number;
+    type: RType;
+    data:T
+}
+
+const responsee:APIResponsee<string> = {
+    status:200,
+    type:RType.SUCCESS, // here it will return the index of SUCCESS in Rtype
+    data:'test'
+}
+
